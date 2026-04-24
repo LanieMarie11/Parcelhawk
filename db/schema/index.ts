dc570571bd7200b896f2321c7aa542af568c4994
LanieMarie11 <info@harmonylandholdings.com>
2026-04-25 01:36:43 +0900

    add buyer_investor_links table and migrated

--- db/schema/index.ts
+++ db/schema/index.ts
@@ -1,3 +1,10 @@
+export {
+  buyerInvestorLinkEndedByEnum,
+  buyerInvestorLinkedViaEnum,
+  buyerInvestorLinkStatusEnum,
+  buyerInvestorLinks,
+  buyerInvestorRealtorFlagEnum,
+} from "./buyer-investor-links";
 export { favorites, savedSearches } from "./favorites-saved-searches";
 export { landListingEmbeddings, landListings } from "./listings";
 export { investors, users } from "./users";
