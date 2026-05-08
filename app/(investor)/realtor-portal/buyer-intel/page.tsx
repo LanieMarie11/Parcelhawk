081512ea2b5771a65ee860acd56bef36235c9236
LanieMarie11 <info@harmonylandholdings.com>
2026-05-09 02:56:32 +0900

    added empty pages

---
+++ app/(investor)/realtor-portal/buyer-intel/page.tsx
@@ -0,0 +1,3 @@
+export default function RealtorPortalBuyerIntelPage() {
+  return <div className="p-4 text-sm text-zinc-600">Buyer Intel page</div>;
+}
---
+++ app/(investor)/realtor-portal/invite-links/page.tsx
@@ -0,0 +1,3 @@
+export default function RealtorPortalInviteLinksPage() {
+  return <div className="p-4 text-sm text-zinc-600">Invite Links page</div>;
+}
